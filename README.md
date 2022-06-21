fd3741f1ec33fbf7487fb0424f7bf643578bcfdc
George Murphy <GeorgeMurphy@Georges-Macbook-Pro-2.local>
2022-06-21 15:03:36 +1000

    Final commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # work-day-scheduler
+Fifth week of the Sydney University coding bootcamp, this challenge consisted of developing a work day scheduler using HTML/CSS and JavaScript.
+
+Within this challenge, we were required to use specific skills learnt in the past week. 
+
+## Acceptance Criteria
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+This is the live URL:  
+
+### Screenshots
+![Screenshot 1](/assets/images/Screenshot-1.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,65 @@
-//when time is past, present or future, the css of the forms input, is changed so that the back ground colour is red, grey or green
+// Assigning variables
+var saveBtn = document.querySelector(".save-btn");
+var container = document.querySelector(".container");
+var time9am = document.getElementById("9am");
+var time10am = document.getElementById("10am");
+var time11am = document.getElementById("11am");
+var time12pm = document.getElementById("12pm");
+var time1pm = document.getElementById("1pm");
+var time2pm = document.getElementById("2pm");
+var time3pm = document.getElementById("3pm");
+var time4pm = document.getElementById("4pm");
+var time5pm = document.getElementById("5pm");
+var text = document.querySelector(".target");
+var date = document.querySelector("#currentDay");
+
+// Time array to iterate through for loop
+var timeArray = [
+    time9am,
+    time10am,
+    time11am,
+    time12pm,
+    time1pm,
+    time2pm,
+    time3pm,
+    time4pm,
+    time5pm,
+]
+
+//when time is past, present or future, the class attribute on the elements are changed so that the back ground colour is red, grey or green
+function checkTime() {
+    today = moment().format("DD/MM/YY");
+    date.textContent = today
+
+    var now = moment().format("kk");
+    for (let i = 0; i < timeArray.length; i++) {
+        if (timeArray[i].classname === "future") {
+            timeArray[i].classList.remove("future");
+        } 
+        if (timeArray[i].classname === "present") {
+            timeArray[i].classList.remove("present");
+        } 
+        if (timeArray[i].classname === "past") {
+            timeArray[i].classList.remove("past");
+        }
+        if (now > timeArray[i].dataset.hour) {
+            timeArray[i].classList.add("past");
+        } else if (now === timeArray[i].dataset.hour) {
+            timeArray[i].classList.add("present");
+        } else {
+            timeArray[i].classList.add("future");
+        }
+    }
+}
+
+// saves task onto local storage
+function saveTask(event) {
+    event.preventDefault();
+    var btnTarget = $(event.currentTarget);
+    text = btnTarget;
+    localStorage.setItem("Schedule", text.val());
+}
+
+checkTime();
+
+saveBtn.addEventListener("click", saveTask);
